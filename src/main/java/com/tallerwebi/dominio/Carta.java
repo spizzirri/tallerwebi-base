9469a18f7e0c3f9c243bdd0038aa1ba17f2d55dc
damian spizzirri <damian.spizzirri@grupoesfera.com.ar>
2024-04-11 20:27:34 -0300

    Ejemplo de TDD con una prueba fallando

---
+++ src/main/java/com/tallerwebi/dominio/Carta.java
@@ -0,0 +1,27 @@
+package com.tallerwebi.dominio;
+
+public class Carta {
+    private int numero;
+    private Palo palo;
+
+    public Carta(int numero, Palo palo){
+        this.numero = numero;
+        this.palo = palo;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public Palo getPalo() {
+        return palo;
+    }
+
+    public void setPalo(Palo palo) {
+        this.palo = palo;
+    }
+}
---
+++ src/main/java/com/tallerwebi/dominio/ServicioChinchon.java
@@ -0,0 +1,18 @@
+package com.tallerwebi.dominio;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ServicioChinchon {
+    public boolean hayChinchon(List<Carta> mano){
+        Carta carta1 = mano.get(0);
+
+        for (Carta cartaActual: mano) {
+            if(!carta1.getPalo().equals(cartaActual.getPalo())){
+                return false;
+            }
+        }
+        return true;
+    }
+}
