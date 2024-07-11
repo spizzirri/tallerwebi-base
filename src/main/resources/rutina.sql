fbe8b557c576c93e5cd7ea6c7357d91f2cbe2094
Santiago <142843041+SantiagoTucci@users.noreply.github.com>
2024-07-11 11:46:10 -0300

    cambios resources

---
+++ src/main/resources/rutina.sql
@@ -0,0 +1,11 @@
+INSERT INTO Rutina (idRutina, nombre, objetivo) VALUES
+    (1, 'Rutina de Hipertrofia', 'GANANCIA_MUSCULAR'),
+    (2, 'Rutina para Quemar Grasa', 'PERDIDA_DE_PESO'),
+    (3, 'Rutina de Definición Muscular', 'DEFINICION'),
+    (4, 'Rutina de Fuerza Explosiva', 'GANANCIA_MUSCULAR'),
+    (5, 'Rutina de Resistencia Cardiovascular', 'PERDIDA_DE_PESO'),
+    (6, 'Rutina de musculación', 'GANANCIA_MUSCULAR'),
+    (7, 'Rutina para Hipopresión', 'DEFINICION'),
+    (8, 'Rutina de Yoga', 'PERDIDA_DE_PESO'),
+    (9, 'Rutina de Pilates para Fortalecer', 'GANANCIA_MUSCULAR'),
+    (10, 'Rutina de Entrenamiento Funcional', 'DEFINICION');
