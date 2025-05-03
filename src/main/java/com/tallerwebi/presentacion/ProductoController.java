75c6c8593da4e8da308f45d30c3ef47c06a6ff92
Macbook <macbook@MacBook-Pro-de-Macbook.local>
2025-05-02 23:07:19 -0300

    agregro a la vista que se renderize la tabla segun los productos que tiene agregados

--- src/main/java/com/tallerwebi/presentacion/ProductoController.java
+++ src/main/java/com/tallerwebi/presentacion/ProductoController.java
@@ -2,7 +2,7 @@ package com.tallerwebi.presentacion;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
@@ -15,15 +15,17 @@ public class ProductoController {
 
     public ProductoController() {
         this.productos = new ArrayList<ProductoDto>();
+        productos.add(new ProductoDto("Coca-Cola", 150.0));
+
     }
 
-    @PostMapping(path = "/carritoDeCompras")
+    @GetMapping(path = "/carrito")
     public ModelAndView mostrarVistaCarritoDeCompras() {
         ModelMap model = new ModelMap();
 
 //        model.put("mensaje", "El producto fue agregado al carrito correctamente!");
-
-        return new ModelAndView("carritoDeCompras", model);
+        model.addAttribute("productos", productos);
+        return new ModelAndView("carrito", model);
 
     }
 }
