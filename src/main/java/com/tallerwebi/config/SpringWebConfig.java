548d108ac2517eeb8b127535c107bc94cdbb4d2d
Mati <matu.dw@gmail.com>
2025-05-09 18:02:33 -0300

    Index todo feo

--- src/main/java/com/tallerwebi/config/SpringWebConfig.java
+++ src/main/java/com/tallerwebi/config/SpringWebConfig.java
@@ -27,6 +27,7 @@ public class SpringWebConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/css/**").addResourceLocations("/resources/core/css/");
         registry.addResourceHandler("/js/**").addResourceLocations("/resources/core/js/");
         registry.addResourceHandler("/webjars/**").addResourceLocations("/webjars/");
+        registry.addResourceHandler("/uploads/**").addResourceLocations("/resources/core/uploads/");
     }
 
     // https://www.thymeleaf.org/doc/tutorials/3.0/thymeleafspring.html
