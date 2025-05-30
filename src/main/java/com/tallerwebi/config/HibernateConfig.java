a534a404efde7ece5f50817b5639b6f95df77247
ger <gemazza@unlam.edu.ar>
2025-05-29 21:17:38 -0300

    Configuracion inicial de hibernate

--- src/main/java/com/tallerwebi/config/HibernateConfig.java
+++ src/main/java/com/tallerwebi/config/HibernateConfig.java
@@ -18,7 +18,11 @@ public class HibernateConfig {
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.hsqldb.jdbcDriver");
+        // dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
         dataSource.setUrl("jdbc:hsqldb:mem:db_");
+        // dataSource.setUrl("jdbc:mysql://localhost:3306/tallerwebi");
+        // dataSource.setUsername("user");
+        // dataSource.setPassword("user");
         dataSource.setUsername("sa");
         dataSource.setPassword("");
         return dataSource;
@@ -41,9 +45,10 @@ public class HibernateConfig {
     private Properties hibernateProperties() {
         Properties properties = new Properties();
         properties.setProperty("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
+        // properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL57Dialect");
         properties.setProperty("hibernate.show_sql", "true");
         properties.setProperty("hibernate.format_sql", "true");
-        properties.setProperty("hibernate.hbm2ddl.auto", "create");
+        properties.setProperty("hibernate.hbm2ddl.auto", "create"); // update
         return properties;
     }
 }
