0799bf01034bf4027b4357779e3140d66c90f86d
Carla Stramandinoli <carla.stram@gmail.com>
2025-06-08 00:06:58 -0300

    agrego la exception necesaria

---
+++ src/main/java/com/tallerwebi/dominio/excepcion/IllegalArgumentException.java
@@ -0,0 +1,8 @@
+package com.tallerwebi.dominio.excepcion;
+
+public class IllegalArgumentException extends Exception {
+
+    public IllegalArgumentException(String mensaje) {
+        super(mensaje);
+    }
+}
