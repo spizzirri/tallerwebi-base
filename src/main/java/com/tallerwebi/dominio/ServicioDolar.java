d1052ddc52619956003416d5353ce4c7a3eac715
LuketoDev <lucasbravouni@gmail.com>
2025-07-02 15:17:33 -0300

    cambia servicio dolar para no sobrecargar la api

--- src/main/java/com/tallerwebi/dominio/ServicioDolar.java
+++ src/main/java/com/tallerwebi/dominio/ServicioDolar.java
@@ -8,16 +8,27 @@ import java.util.Map;
 @Service
 public class ServicioDolar {
 
-    private final RestTemplate restTemplate = new RestTemplate(); //Clase que permite hacer llamdas HTTP a externos
+    private final RestTemplate restTemplate = new RestTemplate();
     private final String API_URL = (System.getenv("API_DOLAR") != null)
             ? System.getenv("API_DOLAR")
             : "https://dolarapi.com/v1/dolares/blue";
 
-    public Double obtenerCotizacionDolarBlue() {
-        Map<String, Object> response = restTemplate.getForObject(API_URL, Map.class); //Hace la solicitud HTTP GET a la URL
-        if (response != null && response.containsKey("venta")) {
-            return Double.parseDouble(response.get("venta").toString());
+    // variables para no pegarle a la api todo el tiempo por cada producto sino cada 10 minutos.
+    private Double cotizacionCacheada = null;
+    private long ultimoUpdate = 0;
+    private static final long DURACION_CACHE_MILLIS = 10 * 60 * 1000; // 10 minutos
+
+    public synchronized Double obtenerCotizacionDolarBlue() {
+        long ahora = System.currentTimeMillis();
+        if (cotizacionCacheada == null || (ahora - ultimoUpdate) > DURACION_CACHE_MILLIS) {
+            Map<String, Object> response = restTemplate.getForObject(API_URL, Map.class);
+            if (response != null && response.containsKey("venta")) {
+                cotizacionCacheada = Double.parseDouble(response.get("venta").toString());
+                ultimoUpdate = ahora;
+            } else {
+                cotizacionCacheada = 0.0; // por si falla la API
+            }
         }
-        return 0.0;
+        return cotizacionCacheada;
     }
 }
