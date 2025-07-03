817bfee842d05f042e3dba7a124685b889f71095
Nicolas Paone <nicolasarielpaone@gmail.com>
2025-07-03 00:15:41 -0300

    Testeos de la API del dolar

--- src/main/java/com/tallerwebi/dominio/ServicioDolar.java
+++ src/main/java/com/tallerwebi/dominio/ServicioDolar.java
@@ -3,19 +3,43 @@ package com.tallerwebi.dominio;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import javax.persistence.Access;
 import java.util.Map;
 
 @Service
 public class ServicioDolar {
 
-    private final RestTemplate restTemplate = new RestTemplate(); //Clase que permite hacer llamdas HTTP a externos
-    private final String API_URL = "https://dolarapi.com/v1/dolares/blue";
+    private final RestTemplate restTemplate; //Clase que permite hacer llamdas HTTP a externos
+    private final String API_URL = (System.getenv("API_DOLAR") != null)
+            ? System.getenv("API_DOLAR")
+            : "https://dolarapi.com/v1/dolares/blue";
 
-    public Double obtenerCotizacionDolarBlue() {
-        Map<String, Object> response = restTemplate.getForObject(API_URL, Map.class); //Hace la solicitud HTTP GET a la URL
-        if (response != null && response.containsKey("venta")) {
-            return Double.parseDouble(response.get("venta").toString());
+    private Double cotizacionCacheada = null;
+    private long ultimoUpdate = 0;
+    private static final long DURACION_CACHE_MILLIS = 10 * 60 * 1000; // 10 minutos
+
+    public ServicioDolar(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    };
+
+    public synchronized Double obtenerCotizacionDolarBlue() {
+        long ahora = System.currentTimeMillis();
+
+        if (cotizacionCacheada == null || (ahora - ultimoUpdate) > DURACION_CACHE_MILLIS) {
+            try {
+                Map<String, Object> response = restTemplate.getForObject(API_URL, Map.class);
+                if (response != null && response.containsKey("venta")) {
+                    cotizacionCacheada = Double.parseDouble(response.get("venta").toString());
+                    ultimoUpdate = ahora;
+                } else {
+                    cotizacionCacheada = 0.0;
+                }
+            } catch (Exception e) {
+                cotizacionCacheada = 0.0;
+            }
         }
-        return 0.0;
+
+        return cotizacionCacheada;
     }
+
 }
