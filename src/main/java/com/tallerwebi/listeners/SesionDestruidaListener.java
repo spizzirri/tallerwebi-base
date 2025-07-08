d78b018b371cb4f85eae1c7c0bb0cd7e86ed468b
LuketoDev <lucasbravouni@gmail.com>
2025-07-08 01:52:51 -0300

    sesion de 15 minutos de duracion hasta que expire

--- src/main/java/com/tallerwebi/listeners/SesionDestruidaListener.java
+++ src/main/java/com/tallerwebi/listeners/SesionDestruidaListener.java
@@ -4,55 +4,43 @@ import com.tallerwebi.context.ApplicationContextProvider;
 import com.tallerwebi.dominio.RepositorioComponente;
 import com.tallerwebi.presentacion.ProductoCarritoDto;
 import org.springframework.context.ApplicationContext;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.WebApplicationContextUtils;
 
-import javax.servlet.ServletContext;
 import javax.servlet.annotation.WebListener;
-import javax.servlet.http.HttpSessionAttributeListener;
-import javax.servlet.http.HttpSessionBindingEvent;
+import javax.servlet.http.*;
 import java.util.List;
 
 
+@WebListener
+public class SesionDestruidaListener implements HttpSessionListener {
+
+    @Override
+    public void sessionCreated(HttpSessionEvent se) {}
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent se) {
+        HttpSession session = se.getSession();
+        List<ProductoCarritoDto> carrito = (List<ProductoCarritoDto>) session.getAttribute("carritoSesion");
+
+        if (carrito != null) {
+            ApplicationContext ctx = ApplicationContextProvider.getApplicationContext();
+            RepositorioComponente repo = ctx.getBean(RepositorioComponente.class);
+
+            if (ctx == null) {
+                System.out.println("ERROR: el WebApplicationContext es null");
+                try {
+                    // Pausa de 5 segundos (10,000 milisegundos)
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return;
+            }
+
+            for (ProductoCarritoDto producto : carrito) {
+                repo.devolverStockDeUnComponente(producto.getId(), producto.getCantidad());
+            }
+        }
+    }
+}
 
 
-//@WebListener
-//public class AtributosSesionListener implements HttpSessionAttributeListener {
-//
-//    @Override
-//    public void attributeAdded(HttpSessionBindingEvent event) {}
-//
-//    @Override
-//    public void attributeReplaced(HttpSessionBindingEvent event){}
-//
-//    @Override
-//    public void attributeRemoved(HttpSessionBindingEvent event) {
-//        String nombre = event.getName();
-//
-//
-//        if (nombre.equals("carritoSesion")){
-//            List<ProductoCarritoDto> carrito = ((List<ProductoCarritoDto>)event.getValue());
-//
-//            // Verificamos el ServletContext
-//            ApplicationContext ctx = ApplicationContextProvider.getApplicationContext();
-//
-//            if (ctx == null) {
-//                System.out.println("ERROR: el WebApplicationContext es null");
-//                try {
-//                    // Pausa de 5 segundos (10,000 milisegundos)
-//                    Thread.sleep(5000);
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                }
-//                return;
-//            }
-//
-//            RepositorioComponente repositorioComponente = ctx.getBean(RepositorioComponente.class);
-//
-//            for (ProductoCarritoDto productoCarrito : carrito) {
-//                repositorioComponente.devolverStockDeUnComponente(productoCarrito.getId(), productoCarrito.getCantidad());
-//            }
-//
-//        }
-//    }
-//}
