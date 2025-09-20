f7f6ee665dd00f39cd478ec3c0cf4aeccadcc2d2
Darío Miguel <dario.e.miguel@gmail.com>
2025-09-20 15:53:29 -0300

    style(ui) Ajustar tamaños en badge-info y badge-wrapper _Desactivar apertura de elemento en endpoint de < 992px

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,14 +7,12 @@ INSERT INTO Usuario(id, email, password, rol, activo) VALUES(null, 'test@unlam.e
 SET FOREIGN_KEY_CHECKS = 0;
 TRUNCATE TABLE Categorias;
 SET FOREIGN_KEY_CHECKS = 1;
-INSERT INTO Categorias(id, nombre, nombreEnUrl) VALUES (null, 'Moda hombre', 'moda-hombre');
-INSERT INTO Categorias(id, nombre, nombreEnUrl) VALUES (null, 'Moda mujer', 'moda-mujer');
-INSERT INTO Categorias(id, nombre, nombreEnUrl) VALUES (null, 'Juguetes y hobbies', 'juguetes-y-hobbies');
-INSERT INTO Categorias(id, nombre, nombreEnUrl) VALUES (null, 'Libros, juegos y música', 'libros-juegos-y-musica');
+INSERT INTO Categorias(id, nombre, nombreEnUrl) VALUES (null, 'Electrónica', 'electronica');
+INSERT INTO Categorias(id, nombre, nombreEnUrl) VALUES (null, 'Moda', 'moda');
+INSERT INTO Categorias(id, nombre, nombreEnUrl) VALUES (null, 'Juegos y pasatiempos', 'juguetes-y-pasatiempos');
 INSERT INTO Categorias(id, nombre, nombreEnUrl) VALUES (null, 'Hogar', 'hogar');
 INSERT INTO Categorias(id, nombre, nombreEnUrl) VALUES (null, 'Oficina', 'oficina');
 INSERT INTO Categorias(id, nombre, nombreEnUrl) VALUES (null, 'Deportes', 'deportes');
 INSERT INTO Categorias(id, nombre, nombreEnUrl) VALUES (null, 'Automotriz', 'automotriz');
-INSERT INTO Categorias(id, nombre, nombreEnUrl) VALUES (null, 'Salud y belleza', 'salud-y-belleza');
 INSERT INTO Categorias(id, nombre, nombreEnUrl) VALUES (null, 'Bebés', 'bebes');
-INSERT INTO Categorias(id, nombre, nombreEnUrl) VALUES (null, 'Electrónica', 'electronica');
+INSERT INTO Categorias(id, nombre, nombreEnUrl) VALUES (null, 'Salud y belleza', 'salud-y-belleza');
