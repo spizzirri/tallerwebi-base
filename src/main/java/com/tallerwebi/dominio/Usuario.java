5234b95dde83131b3b44a342a73f965ff762de6a
NotaMateria <fran.caco.fv@gmail.com>
2025-09-24 21:00:00 -0300

    Ahora

--- src/main/java/com/tallerwebi/dominio/Usuario.java
+++ src/main/java/com/tallerwebi/dominio/Usuario.java
@@ -11,6 +11,7 @@ public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String nombre;
     private String email;
     private String password;
     private String rol;
