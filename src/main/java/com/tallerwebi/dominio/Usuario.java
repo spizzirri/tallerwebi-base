403eb7de9916517fda2894546b13be57873fa282
NotaMateria <fran.caco.fv@gmail.com>
2025-09-24 21:01:19 -0300

    Cambiando el error de prueba

--- src/main/java/com/tallerwebi/dominio/Usuario.java
+++ src/main/java/com/tallerwebi/dominio/Usuario.java
@@ -11,7 +11,6 @@ public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String nombre;
     private String email;
     private String password;
     private String rol;
