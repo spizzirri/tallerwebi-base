45756472c8f38a59044fc552b8c9bbe58e33a532
Agustin Pucci <agustin.pucci.rubio@gmail.com>
2025-10-11 03:53:32 -0300

    Arreglar Servlet error con imagenes

--- src/main/java/com/tallerwebi/MyServletInitializer.java
+++ src/main/java/com/tallerwebi/MyServletInitializer.java
@@ -4,6 +4,8 @@ import com.tallerwebi.config.DatabaseInitializationConfig;
 import com.tallerwebi.config.HibernateConfig;
 import com.tallerwebi.config.SpringWebConfig;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletRegistration;
 
 public class MyServletInitializer
         extends AbstractAnnotationConfigDispatcherServletInitializer {
@@ -24,4 +26,22 @@ public class MyServletInitializer
     protected String[] getServletMappings() {
         return new String[]{"/"};
     }
+
+
+    @Override
+    protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+        registration.setMultipartConfig(getMultipartConfigElement());
+    }
+
+    private MultipartConfigElement getMultipartConfigElement() {
+        MultipartConfigElement multipartConfigElement = new MultipartConfigElement( LOCATION, MAX_FILE_SIZE, MAX_REQUEST_SIZE, FILE_SIZE_THRESHOLD);
+        return multipartConfigElement;
+    }
+
+    private static final String LOCATION            = "C:/temp/";   // Temporary location where files will be stored
+    private static final long MAX_FILE_SIZE         = 5242880;      // 5MB : Max file size - Beyond that size spring will throw exception.
+    private static final long MAX_REQUEST_SIZE      = 20971520;     // 20MB : Total request size containing Multi part.
+    private static final int FILE_SIZE_THRESHOLD    = 0;            // Size threshold after which files will be written to disk
+
+
 }
