846c4a08380ecf9d65ebea0d08980a4adebb10c8
Ivan Ruquet <ivanruquet@gmail.com>
2026-06-24 21:56:05 -0300

    fix: corregir envío de email con MimeMessage y UTF-8

--- src/main/java/com/tallerwebi/dominio/Mail/ServicioEmailImpl.java
+++ src/main/java/com/tallerwebi/dominio/Mail/ServicioEmailImpl.java
@@ -1,14 +1,19 @@
 package com.tallerwebi.dominio.Mail;
 
+import javax.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ServicioEmailImpl implements ServicioEmail {
 
   private final JavaMailSender mailSender;
+
+  @Value("${MAIL_USERNAME}")
+  private String username;
 
   @Autowired
   public ServicioEmailImpl(JavaMailSender mailSender) {
@@ -17,12 +22,16 @@ public class ServicioEmailImpl implements ServicioEmail {
 
   @Override
   public void enviarEmail(String destinatario, String asunto, String mensaje) {
-    SimpleMailMessage mail = new SimpleMailMessage();
-
-    mail.setTo(destinatario);
-    mail.setSubject(asunto);
-    mail.setText(mensaje);
-
-    mailSender.send(mail);
+    try {
+      MimeMessage mail = mailSender.createMimeMessage();
+      MimeMessageHelper helper = new MimeMessageHelper(mail, true, "UTF-8");
+      helper.setFrom(username);
+      helper.setTo(destinatario);
+      helper.setSubject(asunto);
+      helper.setText(mensaje);
+      mailSender.send(mail);
+    } catch (Exception e) {
+      throw new RuntimeException("Error al enviar email", e);
+    }
   }
 }
